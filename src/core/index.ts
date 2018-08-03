d2b12f884791622dd8547f5079fde79d5dc846f8
Felix Khoi <felix@khoi.se>
2018-08-03 17:23:56 -0400

    fix(core-module): Exporting schematics as default

--- src/core/index.ts
+++ src/core/index.ts
@@ -3,12 +3,12 @@ import { normalize } from '@angular-devkit/core';
 import { Schema } from './schema';
 import { getDefaultIndexTS } from '../utility';
 
-export function core(options: Schema): Rule {
+export default function(options: Schema): Rule {
   return chain([
     externalSchematic('@schematics/angular', 'module', options),
     (tree: Tree, _context: SchematicContext) => {
       const parsedPath = normalize(options.path + '/' + options.name);
-      
+
       // Creating barrel for Module
       tree.create(normalize(parsedPath + '/index.ts'), `
 // Services
@@ -35,13 +35,13 @@ export * from './${options.name}.module';
 
       // Creating barrel and folder for all Models
       tree.create(normalize(parsedPath + '/models/index.ts'), getDefaultIndexTS());
-      
+
       // Creating barrel and folder for all Guards
       tree.create(normalize(parsedPath + '/guards/index.ts'), getDefaultIndexTS());
-      
+
       // Creating barrel and folder for all Interceptors
       tree.create(normalize(parsedPath + '/interceptors/index.ts'), getDefaultIndexTS());
-      
+
       // Creating barrel and folder for all Resolvers
       tree.create(normalize(parsedPath + '/resolvers/index.ts'), getDefaultIndexTS());
 
